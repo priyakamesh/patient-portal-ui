5345f479ed28b784f9131da135954a9ca9a1385f
krishnapriya <krishnapriyakamesh@gmail.com>
2017-05-17 09:25:40 -0500

    added responsiveness to the app

--- public/app/controller/profileCtrl.js
+++ public/app/controller/profileCtrl.js
@@ -1,5 +1,11 @@
 patient_portal.controller('ProfileCtrl', function($location,$scope,$http, AuthFactory){
    $('.tooltipped').tooltip({delay: 50});
+   $(".button-collapse").sideNav({
+      menuWidth: 250, // Default is 300
+      edge: 'left', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    });
   $scope.active_portal = "";
 
   $scope.activePortalButton = (id) =>{
